a45476eaa63525bc3e93fdc59962e4af9f19f690
Henry Navntoft <henrylnavntoft@gmail.com>
2023-05-01 13:18:30 +0200

    Fireworks implemented

--- src/pages/thanks/index.js
+++ src/pages/thanks/index.js
@@ -1,14 +1,48 @@
-import Head from "next/head";
+import { Fireworks } from "@fireworks-js/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-export default function ThankYou() {
+export default function App() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.push("/");
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, [router]);
+
   return (
-    <>
-      <Head>
-        <title>Find buyer | EDC</title>
-      </Head>
-      <div className="wrapper">
-        <h1>Thanks</h1>
+    <div style={{ position: "relative", height: "70vh" }}>
+      <Fireworks
+        options={{
+          rocketsPoint: {
+            min: 0,
+            max: 100,
+          },
+        }}
+        style={{
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          position: "fixed",
+          background: "#fff",
+        }}
+      />
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          textAlign: "center",
+          zIndex: 10,
+        }}
+      >
+        <h1>Thank you!</h1>
       </div>
-    </>
+    </div>
   );
 }
