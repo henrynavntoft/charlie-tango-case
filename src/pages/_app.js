6df1f673cc4442be196451dea11ff4bb882216a8
Henry Navntoft <henrylnavntoft@gmail.com>
2023-05-02 11:10:47 +0200

    Preloader made

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,42 @@
 import "@/styles/globals.css";
 import { Header } from "@/components/Header/Header";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Preloader from "@/components/Preloader";
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const handleRouteChangeStart = () => {
+      setIsLoading(true);
+    };
+
+    const handleRouteChangeComplete = () => {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
+    };
+
+    const handleRouteChangeError = () => {
+      setIsLoading(false);
+    };
+
+    router.events.on("routeChangeStart", handleRouteChangeStart);
+    router.events.on("routeChangeComplete", handleRouteChangeComplete);
+    router.events.on("routeChangeError", handleRouteChangeError);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeStart);
+      router.events.off("routeChangeComplete", handleRouteChangeComplete);
+      router.events.off("routeChangeError", handleRouteChangeError);
+    };
+  }, [router]);
+
   return (
     <>
+      {/* {isLoading && <Preloader />} */}
       <Header />
       <main>
         <Component {...pageProps} />
@@ -11,3 +44,5 @@ export default function App({ Component, pageProps }) {
     </>
   );
 }
+
+export default App;
